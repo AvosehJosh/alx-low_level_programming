f91383475a3c5e2dfcf6e4c27928696f27cc6ace
AvosehJosh <avosehjoshua4@gmail.com>
2023-06-15 21:42:27 -0700

    Hello World project

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("our program is up and running\n")
+    return (0);
+}
---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,7 @@
+0. Processor task
+1. Compiler task
+2. Assembler task
+3. Name task
+4. Hello, puts task
+5. Hello, printf task
+6. Size is not grandeur task
